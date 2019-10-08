bab0eb2f936c42489536153380bb84dcd7b84d84
tetsu <banginhoodyonezawa@gmail.com>
2019-10-08 23:06:22 +0900

    Add model User and Post and Category associations spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #　関連付け
+  context 'Assosiations' do
+    # User Assosiation
+    it 'belongs_to user' do
+      association = described_class.reflect_on_association(:user).macro
+      expect(association).to eq :belongs_to
+    end
+
+    # Category Associatin
+    it 'belongs_to Category' do
+      association = described_class.reflect_on_association(:category).macro
+      expect(association).to eq :belongs_to
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
-  # Association
+RSpec.describe User, type: :model do
+  # 関連付け
   context 'Association' do
-    # has_many Post
-    it 'has_many Post' do
+    # has_many posts
+    it 'has_many posts' do
       association = described_class.reflect_on_association(:posts)
       expect(association.macro).to eq :has_many
+      expect(association.options[:dependent]).to eq :destroy
     end
   end
 end
