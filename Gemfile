71cb2d0c43b17f48d59a24b718b02c69560c4563
tetsu <banginhoodyonezawa@gmail.com>
2019-10-12 16:23:44 +0900

    Add hobby and study and team to views/posts/~ brnach etc

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'headless'
 gem 'capybara'
 gem 'database_cleaner'
 gem 'faker'
+gem 'will_paginate', '~> 3.1.0'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,24 @@
 module PostsHelper
+  #views/posts/branch/_create_new_post.html.erb
+  def create_new_post_partial_path
+    if user_signed_in?
+      'posts/branch/create_new_post/signed_in'
+    else
+      'posts/branch/create_new_post/not_signed_in'
+    end
+  end
+
+  # views/posts/branch/_categories.html.erb
+  def all_categories_button_partial_path
+    if params[:category].blank?
+      'posts/branch/categories/all_selected'
+    else
+      'posts/branch/categories/all_not_selected'
+    end
+  end
+
+  # views/posts/_branch.html.erb
+  def no_posts_partial_path
+    @posts.empty? ? 'posts/branch/no_posts' : 'shared/empty_partial'
+  end
 end
