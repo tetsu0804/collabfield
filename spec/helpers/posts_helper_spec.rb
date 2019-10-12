48924d723048e66cc513e6949a242233358fe8d2
tetsu <banginhoodyonezawa@gmail.com>
2019-10-12 19:11:19 +0900

    Add spec/helpers/posts_helper.rb

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -11,5 +11,48 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe PostsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # views/posts/_branch.html.erbのUserがサインインしている場合はCreate New していない場合は Log in画面
+  context '#create_new_post_partial_path' do
+    # Userがサインインしている場合
+    it 'returns signed in partial path' do
+      allow(helper).to receive(:user_signed_in?).and_return(true)
+      expect(helper.create_new_post_partial_path). to eq 'posts/branch/create_new_post/signed_in'
+    end
+
+    # Userがサインインしていない場合
+    it 'returns not signed in partial path' do
+      allow(helper).to receive(:user_signed_in?).and_return(false)
+      expect(helper.create_new_post_partial_path).to eq 'posts/branch/create_new_post/not_signed_in'
+    end
+  end
+
+  # views/posts/branch/_categories.html params[:category]がある場合とない場合
+  context '#all_categories_button_partial_path' do
+    # categoryがない場合
+    it 'returns all_selected partials path' do
+      controller.params[:category] = ''
+      expect(helper.all_categories_button_partial_path).to eq 'posts/branch/categories/all_selected'
+    end
+    # categoryがある場合
+    it 'returns all_not_selected partials path' do
+      controller.params[:category] = 'category'
+      expect(helper.all_categories_button_partial_path).to eq 'posts/branch/categories/all_not_selected'
+    end
+  end
+
+  # views/posts/_branch.html @postsがある場合と無い場合
+  context '#no_posts_partial_path' do
+    # @postsが無い場合
+    it 'returns no_posts partial path' do
+      assign(:posts, [])
+      expect(helper.no_posts_partial_path).to eq 'posts/branch/no_posts'
+    end
+
+    #@postsがある場合
+    it 'returns posts partial path' do
+      assign(:posts, [1])
+      expect(helper.no_posts_partial_path).to eq 'shared/empty_partial'
+    end
+  end
+
 end
