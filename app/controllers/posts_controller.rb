0604b16efd8dfdfeb8c51c1c86b3bde23d4c3a02
tetsu <banginhoodyonezawa@gmail.com>
2019-10-14 15:13:20 +0900

    posts pagination infinite scroll javascript Ajax no code try

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,13 +21,18 @@ class PostsController < ApplicationController
     def posts_for_branch(branch)
       @categories = Category.where(branch: branch)
       @posts = get_posts.paginate(page: params[:page])
+
+      respond_to do |format|
+        format.html
+        format.js { render partial: 'posts/posts_pagination_page' }
+      end
     end
 
     def get_posts
       branch = params[:action]
       category = params[:category]
       search = params[:search]
-      
+
       if category.blank? && search.blank?
         posts = Post.by_branch(branch).all
       elsif category.blank? && search.present?
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -35,4 +35,13 @@ module PostsHelper
       'shared/empty_partial'
     end
   end
+
+  # posts / _posts_pagination_page.js.erb
+  def update_pagination_partial_path
+    if @posts.next_page
+      'posts/posts_pagination_page/update_pagination'
+    else
+      'posts/posts_pagination_page/remove_pagination'
+    end
+  end
 end
