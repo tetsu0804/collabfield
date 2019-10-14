8279af9e1d812afbff65ad2b4f2c588613f7a784
tetsu <banginhoodyonezawa@gmail.com>
2019-10-14 14:19:05 +0900

    Add by_category scope and by_branch scope and search scope to spec/models/post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -24,5 +24,35 @@ RSpec.describe Post, type: :model do
       secound_post = create(:post)
       expect(Post.all).to eq [secound_post, first_post]
     end
+
+    # カテゴリのスコープ
+    it 'by_category scope gets posts by particula category' do
+      category = create(:category)
+      create(:post, category_id: category.id)
+      create_list(:post, 10)
+      posts = Post.by_category(category.branch, category.name)
+      expect(posts.count).to eq 1
+      expect(posts[0].category.name). to eq category.name
+    end
+
+    # アクション(branch)スコープ
+    it 'by_branch scope gets posts by particula branch' do
+      category = create(:category)
+      create(:post, category_id: category.id)
+      create_list(:post, 10)
+      posts = Post.by_branch(category.branch)
+      expect(posts.count).to eq 1
+      expect(posts[0].category.branch).to eq category.branch
+    end
+
+    # サーチのスコープ
+    it 'search finds a matching post' do
+      post = create(:post, title: 'awesome title', content: 'great content' * 5)
+      create_list(:post, 10, title: ('a'..'c' * 2).to_a.shuffle.join)
+      expect(Post.search('awesome').count).to eq 1
+      expect(Post.search('awesome')[0].id).to eq post.id
+      expect(Post.search('great').count).to eq 1
+      expect(Post.search('great')[0].id).to eq post.id
+    end
   end
 end
