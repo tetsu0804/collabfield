48bc799fe9c6a80d69c0cc2640c4bda1643077e9
tetsu <banginhoodyonezawa@gmail.com>
2019-10-15 22:12:13 +0900

    Add constructor app/services PostsForBranchServises

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,19 +29,10 @@ class PostsController < ApplicationController
     end
 
     def get_posts
-      branch = params[:action]
-      category = params[:category]
-      search = params[:search]
-
-      if category.blank? && search.blank?
-        posts = Post.by_branch(branch).all
-      elsif category.blank? && search.present?
-        posts = Post.by_branch(branch).search(search)
-      elsif category.present? && search.blank?
-        posts = Post.by_category(branch, category)
-      elsif category.present? && search.present?
-        posts = Post.by_category(branch, category).search(search)
-      else
-      end
+      PostsForBranchServices.new({
+        search: params[:search],
+        category: params[:category],
+        branch: params[:branch]
+        }).call
     end
 end
