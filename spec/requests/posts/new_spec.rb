c56a94ee47a741c00f6b5e92bc733f5209ebfb8a
tetsu <banginhoodyonezawa@gmail.com>
2019-10-17 20:34:28 +0900

    Add spec/requests/posts/new_spec.rb not_signed_in and signed_in

--- spec/requests/posts/new_spec.rb
+++ spec/requests/posts/new_spec.rb
@@ -1,1 +1,23 @@
 require 'rails_helper'
+include Warden::Test::Helpers
+RSpec.describe "new", :type => :request do
+
+  # サインインしていないユーザー
+  context 'non-signed_in user' do
+    it 'redirect_to user' do
+      get '/posts/new'
+      expect(response).to redirect_to root_path
+    end
+  end
+
+  # サインインしているユーザー
+  context 'signed_in user' do
+    let(:user) { create(:user) }
+    before(:each) { login_as user }
+
+    it 'render a new template' do
+      get '/posts/new'
+      expect(response).to render_template(:new)
+    end
+  end
+end
